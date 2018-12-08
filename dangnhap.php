7f7993328245d2975ca13e16860bee9a0a3fb8d3
anhhieu123 <38438338+anhhieu123@users.noreply.github.com>
2018-12-08 21:58:15 +0700

    Add files via upload

---
+++ dangnhap.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <form action='dangnhap.php?do=login' method='POST'>
+            <table cellpadding='0' cellspacing='0' border='1'>
+                <tr>
+                    <td>
+                        Tên đăng nhập :
+                    </td>
+                    <td>
+                        <input type='text' name='txtUsername' />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Mật khẩu :
+                    </td>
+                    <td>
+                        <input type='password' name='txtPassword' />
+                    </td>
+                </tr>
+            </table>
+            <input type='submit' value='Đăng nhập' />
+            <a href='dangky.php' title='Đăng ký'>Đăng ký</a>
+        </form>
+    </body>
+</html>s
